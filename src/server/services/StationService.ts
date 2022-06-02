bdc20397938123e03503d16239a3b0e013ef049c
Dmitry Kushniruk <kdp19991@gmail.com>
2022-06-03 00:04:06 +0300

    Add StationNetworkConfigCheck handler

--- src/server/services/StationService.ts
+++ src/server/services/StationService.ts
@@ -3,26 +3,41 @@ import * as ping from 'ping';
 
 import type { DB } from '~/server/db';
 import type { BullMQ } from '~/server/lib/bullmq';
+import type { Redis } from '~/server/lib/redis';
 import type { ActivityService } from '~/server/services/ActivityService';
 
 type StationServiceConstructorParams = {
   db: DB;
   bullMQ: BullMQ;
+  redis: Redis;
   activityService: ActivityService;
 };
+
+const TRAEFIK_TCP_ROUTERS_KEYS = {
+  ENTRYPOINTS: 'traefik/tcp/routers/$1/entrypoints/0',
+  RULE: 'traefik/tcp/routers/$1/rule',
+  SERVICE: 'traefik/tcp/routers/$1/service',
+};
+
+const STATION_ORDER_KEY = 'app/station/$1/current-order';
+
+type TRAEFIK_TCP_ROUTERS_KEYS_TYPE = typeof TRAEFIK_TCP_ROUTERS_KEYS;
 
 export class StationService {
   db;
   bullMQ;
+  redis;
   activityService;
 
   constructor({
     db,
     bullMQ,
+    redis,
     activityService,
   }: StationServiceConstructorParams) {
     this.db = db;
     this.bullMQ = bullMQ;
+    this.redis = redis;
     this.activityService = activityService;
   }
 
@@ -34,6 +49,7 @@ export class StationService {
     this.bullMQ.on('repeatable', (job) => {
       if (job.name === 'station-status-check') {
         this._handleStationsStatusUpdate();
+        this._handleStationNetworkConfigCheck();
       }
     });
   }
@@ -88,6 +104,105 @@ export class StationService {
               userId: null,
             });
           }
+        }
+      }),
+    );
+  }
+
+  private async _handleStationNetworkConfigCheck() {
+    const r = this.redis.connection;
+    const prisma = this.db.client;
+
+    const traefikEntryPoints = ['5000', '5001', '5002'];
+    const stationTraefikServices = new Map();
+    (
+      await r.keys('traefik/tcp/services/*/loadBalancer/servers/0/address')
+    ).forEach((key) => {
+      const stationId = key.match(/services\/(.*)\/loadBalancer/)![1];
+      stationTraefikServices.set(stationId, key);
+    });
+
+    const today = new Date();
+    const stations = await prisma.station.findMany({
+      include: {
+        orders: {
+          where: {
+            bookingStartAt: { lte: today },
+            bookingEndAt: { gt: today },
+          },
+        },
+      },
+    });
+    const stationsWithTraefikServiceConfig = stations.filter(({ id }) =>
+      stationTraefikServices.has(id),
+    );
+
+    if (stations.length != stationsWithTraefikServiceConfig.length) {
+      console.warn(
+        'The configuration of traefik services does not match the number of stations added',
+      );
+    }
+
+    const usedEntryPointsKeys = await r.keys(
+      TRAEFIK_TCP_ROUTERS_KEYS.ENTRYPOINTS.replace('$1', '*'),
+    );
+
+    const usedEntryPointsNames = await Promise.all(
+      usedEntryPointsKeys.map((key) => r.get(key)),
+    );
+
+    const availableEntypoins = traefikEntryPoints.filter(
+      (entryPoint) => !usedEntryPointsNames.includes(entryPoint),
+    );
+
+    await Promise.all(
+      stationsWithTraefikServiceConfig.map(async (station) => {
+        const stationConfigKeys = Object.fromEntries(
+          Object.entries(TRAEFIK_TCP_ROUTERS_KEYS).map(([key, traefikKey]) => [
+            key,
+            traefikKey.replace('$1', station.id),
+          ]),
+        ) as TRAEFIK_TCP_ROUTERS_KEYS_TYPE;
+
+        const currentStationOrderIdFromRedis = await r.get(
+          STATION_ORDER_KEY.replace('$1', station.id),
+        );
+
+        if (station.status !== 'USED') {
+          const keys = Object.values(stationConfigKeys);
+          await r.del([...keys, STATION_ORDER_KEY.replace('$1', station.id)]);
+          return;
+        }
+        const currentRouterConfig = await r.keys(
+          `traefik/tcp/routers/${station.id}/*`,
+        );
+
+        if (
+          currentRouterConfig.length ===
+            Object.keys(stationConfigKeys).length &&
+          currentStationOrderIdFromRedis === station.orders[0]!.id
+        ) {
+          return;
+        }
+
+        await r.set(
+          `app/station/${station.id}/current-order`,
+          station.orders[0]!.id,
+        );
+
+        if (availableEntypoins.length) {
+          const config = {
+            [stationConfigKeys.ENTRYPOINTS]: availableEntypoins.pop()!,
+            [stationConfigKeys.RULE]: 'HostSNI(`*`)',
+            [stationConfigKeys.SERVICE]: station.id,
+          };
+          console.log(`Update station config: ${station.name}`, config);
+
+          await Promise.all(
+            Object.entries(config).map(([key, value]) => r.set(key, value)),
+          );
+        } else {
+          // update order status
         }
       }),
     );
